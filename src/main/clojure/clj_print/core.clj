8e3c5bbdbc1a3d50c9705185327e97dd31a89f88
Roberto Acevedo <rxacevedo@gmail.com>
2014-03-28 21:12:56 -0400

    Incremental checkin, rewrote/cleaned up core logic to use nested maps for job-specs, add listeners namespace to hold any future listener implementations.

--- src/main/clojure/clj_print/core.clj
+++ src/main/clojure/clj_print/core.clj
@@ -1,7 +1,8 @@
 (ns ^{:doc "Core print logic."
       :author "Roberto Acevedo"}
   clj-print.core
-  (:require [clj-print [doc-flavors :as flavors]]
+  (:require [clj-print [doc-flavors :as flavors]
+                       [listeners :as listeners]]
             [clojure.java [io :as io]]
             [clojure.pprint :refer [pprint]])
   (:import (java.io File FileInputStream FileNotFoundException)
@@ -16,6 +17,7 @@
                                   HashAttributeSet
                                   HashDocAttributeSet
                                   HashPrintJobAttributeSet
+                                  HashPrintRequestAttributeSet
                                   HashPrintServiceAttributeSet
                                   DocAttribute
                                   PrintJobAttribute
@@ -26,7 +28,8 @@
                                            MediaTray
                                            OrientationRequested
                                            PrinterName
-                                           PrintQuality))
+                                           PrintQuality)
+           (javax.print.event PrintJobListener))
   (:gen-class))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Printers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -36,24 +39,24 @@
    DocFlavor and Attributes acquired from PrintServiceLookup.
    With no arguments, returns a seq of all printers that
    PrintServiceLookup is aware of."
-  {:added "1.0"}
+  {:since "0.0.1"}
   ([& {:keys [^DocFlavor flavor ^AttributeSet attrs]}]
      (seq (PrintServiceLookup/lookupPrintServices flavor attrs))))
 
 (defn printer 
   "Returns the printer with the specified name from PrintServiceLookup.
    With no arguments, returns the system default printer."
-  {:added "1.0"}
-  ([] (PrintServiceLookup/lookupDefaultPrintService))
-  ([name]
-     (if (seq name)
-       (let [attrs (doto (HashAttributeSet.) (.add (PrinterName. name nil)))] 
-         (some (fn [^PrintService p] (when (= name (.. p getName)) p)) (printers nil attrs)))
-       (printer))))
+  {:since "0.0.1"}
+  [name]
+  (cond
+   (= :default name) (PrintServiceLookup/lookupDefaultPrintService)
+   (seq name) (let [attrs (doto (HashAttributeSet.) (.add (PrinterName. name nil)))] 
+                (some (fn [^PrintService p] (when (= name (.. p getName)) p)) (printers nil attrs)))
+   :else nil))
 
 (defn status
   "Returns a seq of this PrintService's status attributes."
-  {:added "1.0"}
+  {:since "0.0.1"}
   [^PrintService p]
   (-> p .getAttributes .toArray seq))
 
@@ -61,121 +64,142 @@
 (defn attributes
   "Returns a flattened seq of this PrintService's supported
   atributes, for all of its supported Attribute classes."
-  {:added "1.0"}
+  {:since "0.0.1"}
   [^PrintService p]
   (let [unflattened (for [c (.. p getSupportedAttributeCategories)]
                       (.. p (getSupportedAttributeValues c nil nil)))] 
     (->> unflattened
-         (map (fn [e] (if (.. (type e) isArray) (seq e) e)))
-         flatten))) ;; TODO: Reflection
+         (map (fn [e] (if (.. ^Class (type e) isArray) (seq e) e)))
+         flatten)))
 
 (defn trays
   "Returns a seq of this PrintService's MediaTrays"
-  {:added "1.0"}
+  {:since "0.0.1"}
   [^PrintService p]
   (filter (fn [attr] (instance? MediaTray attr)) (attributes p)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Util ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- doc-source-type
+(defn- choose-source-key
   "Returns a keyword representative of the document source's
    type."
   {:since "1.0"}
-  [doc-source]
-  (cond (try (.. (io/file doc-source) exists) (catch Throwable t)) :file
-        (try (URL. doc-source) (catch Throwable t)) :url
+  [src]
+  (cond (try (.. (io/file src) exists) (catch Throwable t)) :file
+        (try (URL. src) (catch Throwable t)) :url
         :else nil))
 
-(defn- flavor
+(defn- choose-flavor
   "Attempts to guess the appropriate DocFlavor for the document.
    Returns nil if no suitable DocFlavor is found."
   {:since "1.0"}
-  [doc-source]
-  (condp = (doc-source-type doc-source)
+  [src]
+  (condp = (choose-source-key src)
     :file (:autosense flavors/input-streams)
     :url (:autosense flavors/urls)
     nil))
 
-(defn- setify
-  "Takes a Clojure set and returns an AttributeSet implementation
-   based on bound."
-  [^clojure.lang.IPersistentSet s bound]
-  (if (seq s)
-    (let [el (first s)
-          t (type el)]
-      (condp = bound
-        :service (HashPrintServiceAttributeSet. (into-array t s))
-        :job (HashPrintJobAttributeSet. (into-array t s))
-        :doc (HashDocAttributeSet. (into-array t s))
-        nil))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Job ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defprotocol IPrintable 
-  (submit
-    [this & {:keys [s-attrs]}]
-    "Submits the IPrintable for printing, with optional service attributes."))
-
-;; TODO: Only allow maps
-(defrecord JobMap [doc printer job] 
-  IPrintable
-  (submit [this & {:keys [s-attrs]}]
-    (let [{:keys [attrs ^DocPrintJob obj]} job
-          sdoc (make-doc (:source doc)
-                         (:flavor doc)
-                         (:attrs doc))] ;; TODO: bleh destructure this
-      (.. obj (print @sdoc s-attrs)))))
-
-;; TODO: Create auxiliary fns like job-params/doc-params?
-
-;; TODO: Should printer be optional? If I create these, I can use rest
-;; params and avoid instantiating PersistentHashSets (but then I can't
-;; use literals, hmm)
-
-(defn make-jobmap [doc & {:keys [printer attrs]
-                          :or {printer (printer)
-                               attrs (setify #{MediaTray/MAIN} :job)}}]
-  (->JobMap doc printer {:obj (.. printer createPrintJob)
-                         :attrs attrs}))
-
-(comment "A JobMap might look something like this:"
-         {:doc {:source "path"
-                :flavor (:autosense flavors/input-streams)
-                :attrs #{Chromaticity/MONOCHROME
-                         PrintQuality/HIGH
-                         OrientationRequested/PORTRAIT}}
-          :printer (printer "\\\\srqprint\\2WSouth-Prt3")
-          :attrs #{(Copies. 5) MediaTray/MAIN}})
-
-(defn make-doc
+(defn -make-doc
   "Returns a javax.print.Doc object for the print data in this
    job map. SimpleDoc will throw an IllegalArgumentException if
    the doc-flavor is not representative of the data pointed to
    by doc-source."
-  {:added "1.0"}
-  [source & {:keys [flavor attrs]
-             :or {flavor (flavor source)
-                  attrs (-> #{MediaTray/MAIN} (setify :doc))}}] ;; Pretty sure I need this
-  (let [resource (condp = (doc-source-type source)
-                   :file (FileInputStream. source)
-                   :url (URL. source)
-                   nil)]
-    (delay (SimpleDoc. resource flavor attrs)))) ;; TODO: Do I need the caching provided by this?
+  {:since "0.0.1"}
+  [doc-map]
+  (let [{^String source :source} doc-map
+        {:keys [flavor attrs]
+         :or {flavor (choose-flavor source)
+              attrs (-> #{MediaTray/MAIN} (make-set :doc))}} doc-map ;; Pretty sure I need this
+              resource (condp = (choose-source-key source)
+                         :file (FileInputStream. source)
+                         :url (URL. source)
+                         nil)]
+    ;; TODO: Do I need the caching provided by this?
+    (delay (SimpleDoc. resource flavor (make-set attrs :doc)))))
 
-;; (defn -main [& args]
-;;   (if (seq args)
-;;     (doseq [v args]
-;;       (let [{:keys [doc-source p-name]} (read-string v)
-;;             job (job-map doc-source :p-name p-name)]
-;;         (-> job submit)))))
+(defn- make-set
+  "Takes a Clojure set and returns an AttributeSet implementation
+   based on bound."
+  [^clojure.lang.IPersistentSet s bound]
+  (if (seq s)    
+    (condp = bound        
+      :service (HashPrintServiceAttributeSet. (into-array PrintServiceAttribute s))
+      :job (HashPrintJobAttributeSet. (into-array PrintJobAttribute s))
+      :doc (HashDocAttributeSet. (into-array DocAttribute s))
+      :request (HashPrintRequestAttributeSet. (into-array PrintRequestAttribute s))
+      nil)))
 
-;; This also works!!! Sort of, it causes my printer at home to start
-;; spitting out a bunch of blank pages, not sure why that is yet. 
-(comment (let [f (File. "/Users/Roberto/Dropbox/docs/pdf/semi-log-graph-paper.pdf")
-               b (byte-array (.length f))]
-           (with-open [s (Socket. "10.0.1.14" 9100)
-                       s-stream (.getOutputStream s)
-                       f-stream (FileInputStream. f)]
-             (.read f-stream b)
-             (.write s-stream b)
-             (.flush s-stream))))
+(defn- valid-attrs
+  "Returns true if all of the Attribute objects in
+   attrs are bounded by the type specified by k.
+   Valid keywords for k are:
+     :doc
+     :job
+     :request,
+     :service"
+  {:since "0.0.1"}
+  [attrs k]
+  (condp = k
+    :doc (every? #(instance? DocAttribute %) attrs)
+    :job (every? #(instance? PrintJobAttribute %) attrs)
+    :request (every? #(instance? PrintRequestAttribute %) attrs)
+    :service (every? #(instance? PrintServiceAttribute %) attrs)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Job ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;  A JobMap might look something like this:
+;; 
+;; {:doc {:source "/path/to/doc"
+;;        :flavor (:autosense flavors/input-streams)
+;;        :attrs #{Chromaticity/MONOCHROME
+;;                 PrintQuality/HIGH
+;;                 OrientationRequested/PORTRAIT}}
+;;  :printer (printer "HP_Color_LaserJet_CP3505")
+;;  :attrs #{(Copies. 5) MediaTray/MAIN}
+;;  :listener listeners/basic}
+
+(defn job
+  "Returns a job-spec map that is the result of
+   assoc'ing required values to it. The values are:
+
+   1. A SimpleDoc object (wrapped in a Delay) made from
+   the map keyed at :doc.
+   2. A DocPrintJob object that is retrieved from the
+   PrintService.
+
+   The following defaults are also assoc'd to the job-spec
+   if no values are supplied for them:
+
+   1. The default system printer.
+   2. An attribute set with the single attribute MediaTray/MAIN.
+   3. A basic PrintJobListener that prints any events that occur
+   on the DocPrintJob."
+  {:since "0.0.1"}
+  [job-spec]
+  (if (:doc job-spec)
+    (let [{:keys [doc ^PrintService printer attrs ^PrintJobtListener listener]
+           :or {printer (printer :default)
+                attrs #{MediaTray/MAIN}
+                listener listeners/basic}} job-spec]
+      (if (and (valid-attrs (:attrs (:job job-spec)) :doc)
+               (valid-attrs attrs :job))
+        {:doc (assoc doc :obj (make-doc doc))
+         :printer printer
+         :attrs attrs
+         :listener listener
+         :job (doto (.. printer createPrintJob)
+                (.addPrintJobListener listener))}))))
+
+(defn submit
+  "Submits the job object in job-spec for printing."
+  {:since "0.0.1"}
+  [job-spec]
+  (let [{{obj :obj} :doc} job-spec
+        {:keys [^DocPrintJob job attrs]} job-spec]
+    (.. job (print @obj (make-set attrs :request)))))
+
+(defn -main [& args]
+  (if (seq args)
+    (doseq [v args]
+      (let [job-spec (read-string v)]
+        (-> (job job-spec) submit)))))
--- src/main/clojure/clj_print/listeners.clj
+++ src/main/clojure/clj_print/listeners.clj
@@ -4,11 +4,15 @@
   (:import (javax.print.event PrintJobListener))
   (:gen-class))
 
+(def counter (atom 0))
+
 (def ^{:doc "Basic PrintJobListener implementation."}
-  basic-job-listener
+  basic
   (reify PrintJobListener
     (printDataTransferCompleted [this pje]
-      (println (str "Transfer complete: " pje)))
+      (println (str "Transfer complete: " pje))
+      (println @counter)
+      (swap! counter inc))
     (printJobCompleted [this pje]
       (println (str "Job complete: " pje)))
     (printJobFailed [this pje]
@@ -16,6 +20,6 @@
     (printJobCanceled [this pje]
       (println (str "Job canceled: " pje)))
     (printJobNoMoreEvents [this pje]
-      (println (str "No more events for: " pje)))
+      (println (str "No more events: " pje)))
     (printJobRequiresAttention [this pje]
       (println (str "Job requires attention: " pje)))))
